e1318b8b173e6ae1aaf0529f4eb9f2df85bcfbfb
azzamaflah <azzamhebat46@gmail.com>
2025-01-12 00:22:35 +0700

    untuk mengelola logika input data mahasiswa

---
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InserViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.firebase.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.firebase.repository.RepositoryMhs
+
+class InsertViewModel(
+    private val mhs: RepositoryMhs
+) : ViewModel() {
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
