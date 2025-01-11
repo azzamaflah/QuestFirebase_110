597bd4083bb770fb67508d310088e7a1b5bfc0b2
azzamaflah <azzamhebat46@gmail.com>
2025-01-12 00:36:28 +0700

    untuk menangani input data mahasiswa, termasuk pengelolaan state

---
+++ app/src/main/java/com/example/firebase/ui/view/InsertView.kt
@@ -0,0 +1,47 @@
+package com.example.firebase.ui.view
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase.ui.viewmodel.FormState
+import com.example.firebase.ui.viewmodel.InsertViewModel
+import com.example.firebase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState // State utama untuk loading, success, error
+    val uiEvent = viewModel.uiEvent // State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println("InsertMhsView: uiState is FormState.Success, navigate to home ${uiState.message}")
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) // Tampilkan snackbar
+                }
+                delay(700)
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
