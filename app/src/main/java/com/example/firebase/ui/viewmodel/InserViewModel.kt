021a03a270800162a7d80c7a0dbad467b9394766
azzamaflah <azzamhebat46@gmail.com>
2025-01-12 00:51:27 +0700

    fix jenisKelamin menjadi gender

--- app/src/main/java/com/example/firebase/ui/viewmodel/InserViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InserViewModel.kt
@@ -30,7 +30,7 @@ class InsertViewModel(
         val errorState = FormErrorState(
             nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
             nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
-            jenisKelamin = if (event.gender.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            gender = if (event.gender.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
             alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
             kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
             angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
@@ -83,13 +83,13 @@ data class InsertUiState(
 data class FormErrorState(
     val nim: String? = null,
     val nama: String? = null,
-    val jenisKelamin: String? = null,
+    val gender: String? = null,
     val alamat: String? = null,
     val kelas: String? = null,
     val angkatan: String? = null
 ) {
     fun isValid(): Boolean {
-        return nim == null && nama == null && jenisKelamin == null &&
+        return nim == null && nama == null && gender == null &&
                 alamat == null && kelas == null && angkatan == null
     }
 }
